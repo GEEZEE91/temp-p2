ff77343ba8a8480595d7d72fa378915fb64aed4a
GEEZEE91 <GINA.ZIVKOVIC91@GMAIL.COM>
2022-11-25 03:53:48 +0800

    g

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,9 +2,16 @@ module.exports = {
   format_time: (date) => {
     return date.toLocaleTimeString();
   },
+<<<<<<< Updated upstream
   format_date: (date) => {
     return `${new Date(date).getDate()}/${new Date(date).getMonth()}/${
       new Date(date).getFullYear()
     }`;
   },
 };
+=======
+
+    format_date: (date) => {
+      return ` ${new Date(date).getDate()} /${new Date(date).getMonth()} / ${new Date(date).getFullYear()}`;
+    }};
+>>>>>>> Stashed changes
